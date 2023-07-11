cbbd8f1b6ed7ff0364852989d916ee60b5667d69
Sugeiri58 <danielacastilko@gmail.com>
2023-07-11 07:58:17 -0600

    first commit

--- daniapw/src/app/services/firebase/auth.js
+++ daniapw/src/app/services/firebase/auth.js
@@ -17,4 +17,23 @@ function login(email, password) {
   });
 }
 
-export { login }
+
+function registrer(email, password){
+  const auth = getAuth(app);
+
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential)=> {
+    // Signed in
+    const user = userCredential.user;
+    console.log(UserCredential)
+    // ...
+  })
+  .catch(error => {
+    const errorCode = error.code;
+    const errorMessage =error.message;
+    console.log(errorCode, errorMessage)
+    // .. 
+  });
+}
+
+export { login, registrer }
